7c760a9ee05e4f54b7afda44126f55e94ffa3511
Nadeem Shaikh <112391206+5452014@users.noreply.github.com>
2023-12-14 11:38:52 +0530

    Added all necessary files and folder

---
+++ index.js
@@ -0,0 +1,65 @@
+var btnlength=document.querySelectorAll('.drum').length
+for (let i = 0; i < btnlength; i++) {
+    var btn=document.querySelectorAll('.drum').innerHTML;
+    document.querySelectorAll('.drum')[i].addEventListener('click',function(){
+    
+  var btnHTML=this.innerHTML;
+   AudioPlay(btnHTML);
+   btnAnimation(btnHTML);
+})
+}
+
+document.addEventListener('keypress',function(event){
+  var key = event.key;
+  AudioPlay(key);
+  btnAnimation(key);
+});
+
+
+function AudioPlay(key){
+  switch (key) {
+    case "w":
+      var audio = new Audio('sounds/tom-1.mp3');
+      audio.play();
+
+      break;
+    case "a":
+      var audio = new Audio('sounds/tom-2.mp3');
+      audio.play();
+
+    case "s":
+      var audio = new Audio('sounds/tom-3.mp3');
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio('sounds/tom-4.mp3');
+      audio.play();
+      break;
+
+    case "j":
+      var audio = new Audio('sounds/snare.mp3');
+      audio.play();
+      break;
+
+    case "k":
+      var audio = new Audio('sounds/crash.mp3');
+      audio.play();
+      break;
+
+    case "l":
+      var audio = new Audio('sounds/kick-bass.mp3');
+      audio.play();
+      break;
+    default:
+  }
+}
+
+
+function btnAnimation(eventkey){
+  var activebtn=document.querySelector('.'+eventkey);
+   activebtn.classList.add('pressed')
+  setTimeout(function () {
+    activebtn.classList.remove('pressed')
+  }, 100);
+
+}
